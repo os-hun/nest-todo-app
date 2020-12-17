9785ff5a91b1cd477f6fc3412a8f15d11e8bf538
Shun <shun12baseball@gmail.com>
2020-12-17 12:09:07 +0900

    feat: add views, public/css, public/image, create views/index.hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/new')
+  @Render('index.hbs')
+  todoNew() {
+    return { title: 'Nest Todo App' };
+  }
+
+  @Get('/create')
+  create(@Res() res) {
+    return res.status(302).redirect('/');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,23 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  app.useStaticAssets({
+    root: join(__dirname, '../src', 'public'),
+    prefix: '/public/',
+  });
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, '../src', 'views'),
+  });
   await app.listen(3000);
 }
 bootstrap();
